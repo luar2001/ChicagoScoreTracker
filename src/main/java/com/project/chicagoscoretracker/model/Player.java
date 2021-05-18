b9ce312ac64fc3f50f631f47e0c5363fdf2de668
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-05-18 16:48:27 +0200

    Created a Player Model

---
+++ src/main/java/com/project/chicagoscoretracker/model/Player.java
@@ -0,0 +1,45 @@
+package com.project.chicagoscoretracker.model;
+
+import lombok.Data;
+
+/**
+ * Created by Lukas Aronsson
+ * Date: 18/05/2021
+ * Time: 16:04
+ * Project: ChicagoScoreTracker
+ * Copyright: MIT
+ **/
+@Data
+public class Player {
+
+    /**
+     * player id
+     */
+    private Integer id;
+
+    /**
+     * Name of the player
+     */
+    private String name;
+
+    /**
+     * Number of times this player has won
+     */
+    private int wins = 0;
+
+    /**
+     * Players score in current game
+     * <br>
+     * resets to 0 when game is complete
+     */
+    private int score = 0;
+
+
+    /**
+     * Constructor for Player
+     * @param name Player name
+     */
+    public Player(String name){
+        this.name =name;
+    }
+}
