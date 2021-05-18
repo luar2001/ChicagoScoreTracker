141fe886c2c9ff52f6b4d82a60529dd4485ba935
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-05-18 14:56:04 +0200

    added lombok to Settings.java

--- src/main/java/com/project/chicagoscoretracker/Settings.java
+++ src/main/java/com/project/chicagoscoretracker/Settings.java
@@ -1,5 +1,9 @@
 package com.project.chicagoscoretracker;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * Created by Lukas Aronsson
  * Date: 18/05/2021
@@ -7,6 +11,9 @@ package com.project.chicagoscoretracker;
  * Project: ChicagoScoreTracker
  * Copyright: MIT
  **/
+@Getter
+@Setter
+@NoArgsConstructor
 public class Settings {
 
     /**
@@ -14,48 +21,48 @@ public class Settings {
      * <br>
      * default value = 52
      */
-    int winScore = 52;
+    private int winScore = 52;
 
     /**
      * the score where the player in question is no longer allowed to change there cards.
      * <br>
      * default value = 41
      */
-    int buyStop = 41;
+    private int buyStop = 41;
 
     /**
      * the score for wining a round
      * <br>
      * default value = 5
      */
-    int round = 5;
+    private int round = 5;
 
     /**
      * score for wining a round with a 2
      * <br>
      * default value = 15
      */
-    int endWith2 = 15;
+    private int endWith2 = 15;
 
     /**
      * score threshold where the player is allowed to chicago
      * <br>
      * default value = 15
      */
-    int scoreToChicago = 15;
+    private int scoreToChicago = 15;
 
     /**
      * score for wining a chicago
      * <br>
      * default value = 15
      */
-    int chicago = 15;
+    private int chicago = 15;
 
     /**
      * score the player losses when the player losses after saying chicago
      * <br>
      * default value = -15
      */
-    int chicagoLoss = -15;
+    private int chicagoLoss = -15;
 
 }
