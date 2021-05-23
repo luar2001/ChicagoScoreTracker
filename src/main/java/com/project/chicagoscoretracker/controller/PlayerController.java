2b4b4f3b62cc1c9a9b30a5cb504b83e6d6d341ce
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-05-23 13:47:30 +0200

    Created PlayerController.java

---
+++ src/main/java/com/project/chicagoscoretracker/controller/PlayerController.java
@@ -0,0 +1,44 @@
+package com.project.chicagoscoretracker.controller;
+
+import com.project.chicagoscoretracker.model.Player;
+import com.project.chicagoscoretracker.service.PlayerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by Lukas Aronsson
+ * Date: 23/05/2021
+ * Time: 13:20
+ * Project: ChicagoScoreTracker
+ * Copyright: MIT
+ **/
+@RequiredArgsConstructor
+@RestController
+public class PlayerController {
+
+    private final PlayerService service;
+
+    @GetMapping("/player")
+    public List<Player> getPlayers(){return service.getPlayers();}
+
+    @PostMapping("/player")
+    public void savePlayer(@RequestBody Player player){service.savePlayer(player);}
+
+    @GetMapping("/player/name")
+    public Player byName(String name){return service.getPlayerByName(name);}
+
+    @GetMapping("/player/id")
+    public Player byId(int id){return service.getPlayerById(id);}
+
+    @GetMapping("/player/wins")
+    public List<Player> getPlayerByWins(int wins){return service.getPlayerByWins(wins);}
+
+    @PostMapping("/player")
+    public void deletePlayer(@RequestBody int id){service.deletePlayer(id);}
+
+}
