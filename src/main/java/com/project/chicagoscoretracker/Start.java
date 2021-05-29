d5a72490f9ef51b1c1a0c2a60d46403ea39e3937
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-05-29 23:36:19 +0200

    Made Start.java hold the game loop

--- src/main/java/com/project/chicagoscoretracker/Start.java
+++ src/main/java/com/project/chicagoscoretracker/Start.java
@@ -3,6 +3,8 @@ package com.project.chicagoscoretracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 /**
  * Created by Lukas Aronsson
  * Date: 26/05/2021
@@ -14,8 +16,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Start {
     public static void main(String[] args) {
         SpringApplication.run(Start.class, args); // runs spring!
-        Commands commands = new Commands();
-        commands.start();
+        Scanner scan = new Scanner(System.in);
+        Commands command = new Commands();
+        System.out.println("\nCHICAGO");
+        command.addCommands();
+        while (true){ //program just runs forever until you shout it down.
+            System.out.println("\nInput command: ");
+            command.commandHandler(scan.nextLine());
+        }
 
     }
 }
